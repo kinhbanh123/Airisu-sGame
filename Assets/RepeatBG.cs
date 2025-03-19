ac7dfcec489adc8247a2c6974255020e441853f6
kinhbanh123 <tnguyenbinhxyz@gmail.com>
2025-03-19 11:38:03 +0700

    Fix

---
+++ Assets/RepeatBG.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class RepeatBG : MonoBehaviour
+{
+    private float repeatHeight;
+    private Vector3 startPos;
+
+    [SerializeField] private float speed = 100f; 
+
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        startPos = rectTransform.anchoredPosition;
+        repeatHeight = rectTransform.rect.height; 
+    }
+
+    void Update()
+    {
+        // Di chuyển xuống
+        rectTransform.anchoredPosition += Vector2.down * speed * Time.deltaTime;
+
+        // Kiểm tra nếu BG ra khỏi màn hình, đưa lên lại
+        if (rectTransform.anchoredPosition.y <= startPos.y - repeatHeight)
+        {
+            rectTransform.anchoredPosition += new Vector2(0, repeatHeight * 2);
+        }
+    }
+}
